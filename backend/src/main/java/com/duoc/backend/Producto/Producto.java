fc6746b77338bad3d6e870941b9590789a821216
MAHS-coding <mahscoding@gmail.com>
2025-09-28 03:47:11 -0300

    Lombock
    Agragado lombock a las dependencias

--- backend/src/main/java/com/duoc/backend/Producto/Producto.java
+++ backend/src/main/java/com/duoc/backend/Producto/Producto.java
@@ -1,60 +1,17 @@
-package com.duoc.backend.Producto;
+package com.duoc.pasteleria.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 public class Producto {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private LocalDate date;
-    private LocalTime time;
-    private String reason;
-    private String veterinarian;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public void setTime(LocalTime time) {
-        this.time = time;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public String getVeterinarian() {
-        return veterinarian;
-    }
-
-    public void setVeterinarian(String veterinarian) {
-        this.veterinarian = veterinarian;
-    }
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int stock;
 }
