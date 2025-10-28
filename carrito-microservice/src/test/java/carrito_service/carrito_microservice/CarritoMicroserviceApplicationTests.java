9b66363cecb550a9abf26e7abb4442afcf90b7c7
GustavoAlvial <gu.alvial@duocuc.cl>
2025-10-28 00:28:49 -0300

    Reestructuración y separación de microservicios

---
+++ carrito-microservice/src/test/java/carrito_service/carrito_microservice/CarritoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package carrito_service.carrito_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ core-microservice/src/test/java/core_service/core_microservice/CoreMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package core_service.core_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
